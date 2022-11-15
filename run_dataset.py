6b87f513af05b7472e70a16edcacd4c4f33ab6dd
s-gorrin <sgorrin@gmail.com>
2022-11-15 11:35:09 -0800

    Add more control to the function call

--- run_dataset.py
+++ run_dataset.py
@@ -43,27 +43,31 @@ def preprocessing(some_data):
     return updated_data, index_table, base_fuzzy
 
 
-def run_dataset(some_data, index_table, base_fuzzy):
+def run_dataset(some_data, index_table, base_fuzzy, points=2, close_threshold=0.1, start=0, step=1):
     """
     INTENT: the procedural work of running a full set of tests on a dataset and printing results
+
+    The arguments with default values have been added to give more control to the calling code,
+        which is particularly useful for jupyter notebooks.
+
+    start and step are used in the range() for which lines of the dataset to run.
 
     This uses the get_alpha sorted version
     """
     # print some_data info
     print(f"data shape: {some_data.shape}")
-    print(f"base fuzzy: {base_fuzzy}")
+    # print(f"base fuzzy: {base_fuzzy}")
 
     length = some_data.shape[0]
 
-    points = 2
-    close_threshold = 0.5
     close = 0
     close_lines = []
-    print_lines = False
+    print_lines = True
+    lines_run = 0
 
     print(f'points sought: {points}, threshold for "close result": {close_threshold}')
 
-    for i in range(length):
+    for i in range(start, length, step):
         trimmed_data, trimmed_table, test = extract_test_line(some_data, index_table, i)
 
         alpha, indices = get_alpha(test[0], trimmed_data, trimmed_table, base_fuzzy, points, max_iterations=10)
@@ -82,4 +86,6 @@ def run_dataset(some_data, index_table, base_fuzzy):
             print(f"{i:4}) alpha: {alpha:.3f}, points: {len(indices):2}\t"
                   f"output: {output:.3f}\ttarget: {test[1]:.3f}\tdiff: {difference:.3f}{end}")
 
-    print(f"{close} close results of {length}, or {(close / length) * 100:.2f}%")
+        lines_run += 1
+
+    print(f"{close} close results of {lines_run} lines, or {(close / lines_run) * 100:.2f}%")
